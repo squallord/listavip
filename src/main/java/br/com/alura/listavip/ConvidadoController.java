8132f602e5cc91eb4d8fca9591bdf6967894228d
Diego Marcílio <diego@localhost.localdomain>
2019-08-23 18:02:46 -0300

    alura spring-boot course end

--- src/main/java/br/com/alura/listavip/ConvidadoController.java
+++ src/main/java/br/com/alura/listavip/ConvidadoController.java
@@ -3,16 +3,18 @@ package br.com.alura.listavip;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import br.com.alura.listavip.model.Convidado;
-import br.com.alura.listavip.repository.ConvidadoRepository;
+import br.com.alura.listavip.service.ConvidadoService;
 
 @Controller
 public class ConvidadoController {
 
 	@Autowired
-	private ConvidadoRepository repository;
+	ConvidadoService service;
 	
 	@RequestMapping(path = "/")
 	public String index() {
@@ -21,9 +23,16 @@ public class ConvidadoController {
 	
 	@RequestMapping(path = "/listaconvidados")
 	public String listaConvidados(Model model) {
-		Iterable<Convidado> convidados = repository.findAll();
-		model.addAttribute("convidados", convidados);
+		model.addAttribute("convidados", service.obterTodos());
+		model.addAttribute("convidado", new Convidado());
 		return "listaconvidados";
+	}
+	
+	@RequestMapping(value = "salvar", method = RequestMethod.POST)
+	public String salvar(@ModelAttribute Convidado convidado, Model model) {
+		service.salvar(convidado);
+//		new EmailService().enviar(convidado.getNome(), convidado.getEmail());
+		return listaConvidados(model);
 	}
 	
 }
---
+++ src/main/java/br/com/alura/listavip/service/ConvidadoService.java
@@ -0,0 +1,23 @@
+package br.com.alura.listavip.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.alura.listavip.model.Convidado;
+import br.com.alura.listavip.repository.ConvidadoRepository;
+
+@Service
+public class ConvidadoService {
+
+	@Autowired
+	ConvidadoRepository repository;
+	
+	public Iterable<Convidado> obterTodos() {
+		return repository.findAll();
+	}
+	
+	public void salvar(Convidado convidado) {
+		repository.save(convidado);
+	}
+	
+}
